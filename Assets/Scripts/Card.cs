56e24311b195c104b81c5456a22d49775c4ec166
Meher-StolenPad <meher.jouini@stolenpad-studio.com>
2024-05-18 08:36:48 +0100

    Add card interaction + Card flip animation

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,22 +11,55 @@ namespace Davanci
     public class Card : MonoBehaviour
     {
         [SerializeField] private CanvasGroup CanvasGroup;
-        [SerializeField] private Image BackCard;
-        [SerializeField] private Image FaceCard;
+        [SerializeField] private RectTransform CardHolder;
+        [SerializeField] private Image BackCardImage;
+        [SerializeField] private Image FaceCardImage;
 
-        private int id;
-            
+        private int Id;
+        private bool CardState;
+
         internal void Init(int _id, Sprite _face)
         {
-            id = _id;
-            FaceCard.sprite = _face;
+            Id = _id;
+            FaceCardImage.sprite = _face;
         }
         internal void DisableCard()
         {
-            id = -1;
+            Id = -1;
             CanvasGroup.alpha = 0;
             CanvasGroup.interactable = false;
             CanvasGroup.blocksRaycasts = false;
+        }
+        //function called from UI
+        public void OnCardClicked()
+        {
+            if (CardState) return;
+            ShowCard();
+        }
+        public void ShowCard()
+        {
+            CardState = true;
+            CardHolder.DOLocalRotate(Vector3.up * 90f, 0.3f, RotateMode.Fast).OnComplete(() =>
+            {
+                BackCardImage.gameObject.SetActive(false);
+                FaceCardImage.gameObject.SetActive(true);
+                CardHolder.DOLocalRotate(Vector3.up * 180f, 0.3f, RotateMode.Fast).OnComplete(() =>
+                {
+                    HideCard();
+                });
+            });
+        }
+        internal void HideCard()
+        {
+            CardHolder.DOLocalRotate(Vector3.up * 90f, 0.3f, RotateMode.Fast).SetDelay(2f).OnComplete(() =>
+            {
+                BackCardImage.gameObject.SetActive(true);
+                FaceCardImage.gameObject.SetActive(false);
+                CardHolder.DOLocalRotate(Vector3.zero, 0.3f, RotateMode.Fast).OnComplete(() =>
+                {
+                    CardState = false;
+                });
+            });
         }
     }
 
