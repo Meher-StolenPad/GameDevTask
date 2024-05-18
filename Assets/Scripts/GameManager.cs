cac563aa57396da0a60643dac648a39dd005abd1
Meher-StolenPad <meher.jouini@stolenpad-studio.com>
2024-05-18 09:38:28 +0100

    Add UI Manager
    - Add UI to show time,moves count and match Count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Davanci
@@ -7,7 +8,21 @@ namespace Davanci
     {
         private const float DelayBetweenCards = 0.1f;
 
+        #region Callback Region
         internal static Action<Card> OnCardFlippedCallback;
+        internal static Action<int> OnTick;
+        internal static Action<int, bool> OnMoveCallback;   
+        internal static Action<int> OnCardMatchedCallback;
+
+        #endregion
+
+        #region Private Variables Region
+        private float timeSinceLastTick = 0f;
+        private int TimeSinceStarted;
+
+        private int MatchCount; 
+        private int MovesCount;
+        #endregion
 
         private void OnEnable()
         {
@@ -21,8 +36,14 @@ namespace Davanci
         }
         private void OnCardsCompared(bool _isMatch, Card _card1, Card _card2)
         {
+            MovesCount++;
+
             if (_isMatch)
             {
+                MatchCount++;
+
+                OnCardMatchedCallback?.Invoke(MatchCount);
+
                 _card1.CollectCard();
                 _card2.CollectCard(DelayBetweenCards);
             }
@@ -31,11 +52,22 @@ namespace Davanci
                 _card1.HideCard();
                 _card2.HideCard();
             }
+            OnMoveCallback?.Invoke(MovesCount, _isMatch);
         }
         private void OnDisable()
         {
             OnCardFlippedCallback -= OnCardFlipped;
             CardComparer.m_OnCardsCompared -= OnCardsCompared;
+        }
+        private void Update()
+        {
+            timeSinceLastTick += Time.deltaTime;
+            if (timeSinceLastTick >= 1f)
+            {
+                timeSinceLastTick -= 1f;
+                TimeSinceStarted++;
+                OnTick?.Invoke(TimeSinceStarted);
+            }
         }
     }
 }
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,20 +1,32 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Davanci
 {
     public static class Utils
     {
-        public static void Shuffle<T>(this T[] array)
+        public static void Shuffle<T>(this T[] _array)
         {
-            for (int i = array.Length - 1; i > 0; i--)
+            for (int i = _array.Length - 1; i > 0; i--)
             {
                 int randomIndex = Random.Range(0, i + 1);
-                T temp = array[i];
-                array[i] = array[randomIndex];
-                array[randomIndex] = temp;
+                T temp = _array[i];
+                _array[i] = _array[randomIndex];
+                _array[randomIndex] = temp;
             }
         }
+        public static void SetTimeText(this TextMeshProUGUI _textMeshProUGUI, int _time)
+        {
+            var minutes = _time / 60;
+            var seconds = _time % 60;
+
+            _textMeshProUGUI.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
+        }
+        public static void SetTextAnimated(this TextMeshProUGUI textMeshProUGUI, string text)
+        {
+            textMeshProUGUI.text = text;
+            textMeshProUGUI.transform.DOPunchScale(Vector3.one * 0.3f, 0.3f);
+        }
     }
-
-
 }
